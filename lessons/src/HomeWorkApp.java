09bff250fb0adafbd46aa9a97bf280094307158c
RomanMileshko <r0mm@yandex.ru>
2021-07-23 02:59:25 +0300

    lesson-4

--- lessons/src/HomeWorkApp.java
+++ lessons/src/HomeWorkApp.java
@@ -1,177 +1,238 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class HomeWorkApp {
+    static final char emptyChar = '-';
+    static final int  countBitForWin = 4;
+    static final char signAI = 'O';
+    static final char signHuman = 'X';
+
     public static void main(String[] args) {
 
-        System.out.println("(#1) ---------------------------------------");
-        int[] invArr = { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
-        invertArray(invArr);
+        int sizeOfMatrix = setSizeOfMatrix();
 
-        System.out.println("\n\n(#2) ---------------------------------------");
-        print1DArray(fillArray(100));
-
-        System.out.println("\n(#3) ---------------------------------------");
-        int[] chgArr = {  1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1  };
-        changeArray(chgArr);
-
-        System.out.println("\n(#4) ---------------------------------------");
-        fillDiagonal(4);
-
-        System.out.println("\n(#5) ---------------------------------------");
-        int[] initArr = getInitArray(11, 101);
-        print1DArray(initArr);
-
-        System.out.println("\n(#6) ---------------------------------------");
-        int[] minMaxArr = {20, 25, 1, 15, 60, 75, 30, 21};
-        printMinMax(minMaxArr);
-
-        System.out.println("\n(#7) ---------------------------------------");
-        int[] balanceArr = {2, 2, 2, 1, 2, 2, 10, 1};
-        System.out.println(checkBalance(balanceArr));
-
-        System.out.println("\n(#8) ---------------------------------------");
-        int[] shiftArr = {6, 1, 3, 5};
-        shiftArray(shiftArr, 2);
+        if (sizeOfMatrix != 0)
+            ticTacToe(sizeOfMatrix);
     }
 
-//  1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
+    static void ticTacToe(int sizeOfMatrix) {
+        char[][] matrix = setMatrixEmpty(sizeOfMatrix);
+        drawMatrix(matrix);
+        do {
+            if (!movePlayer(matrix)) break;
+            drawMatrix(matrix);
+            if (isWin(matrix, signHuman)) {
+                System.out.println("Your are win!");
+                break;
+            }
+            if (isDraw(matrix)) {
+                System.out.println("This is draw. ");
+                break;
+            }
 
-    public static void invertArray(int[] arr) {
+            moveAI(matrix);
+            if (isWin(matrix, signAI)) {
+                System.out.println("AI win !");
+                break;
+            }
+            if (isDraw(matrix)) {
+                System.out.println("This is draw. ");
+                break;
+            }
 
-        print1DArray(arr);
+        } while(true);
+    }
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = arr[i] ^ 1;
-            System.out.print(arr[i] + " ");
+    static boolean isDraw(char[][] matrix) {
+        for (int y = 0; y < matrix.length; y++) {
+            for (int x = 0; x < matrix.length; x++) {
+                if (isEmptyCell(matrix, x, y))
+                    return false;
+            }
         }
+        return true;
     }
 
-//  2. Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 … 100;
+    static boolean isWin(char[][] matrix, char sign) {
+        int lineWin, contrLineWin;
 
-    public static int[] fillArray(int len) {
-        int[] arr = new int[len];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i + 1;
-        }
-        return arr;
-    }
+//        Если матрица 3*3, то классически проверяем вертикали-горизонтали, затем диагонали
+        if (matrix.length == 3) {
+            for (int y = 0; y < matrix.length; y++) {
+                lineWin = 0;
+                for (int x = 0; x < matrix.length; x++)
+                    if (matrix[y][x] == sign) lineWin++;
+                if (lineWin == matrix.length) return true;
+            }
 
-//  3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
+            for (int x = 0; x < matrix.length; x++) {
+                lineWin = 0;
+                for (int y = 0; y < matrix.length; y++)
+                    if (matrix[y][x] == sign) lineWin++;
+                if (lineWin == matrix.length) return true;
+            }
 
-    public static void changeArray(int[] arr) {
+            lineWin = 0;
+            for (int xy = 0; xy < matrix.length; xy++)
+                if (matrix[xy][xy] == sign) lineWin++;
+            if (lineWin == matrix.length) return true;
 
-        formatPrint1DArray(arr);
+            lineWin = 0;
+            for (int xy = 0; xy < matrix.length; xy++)
+                if (matrix[(matrix.length - 1) - xy][xy] == sign) lineWin++;
+            return lineWin == matrix.length;
 
-        for (int i = 0; i < arr.length; i++)
-            if (arr[i] < 6) arr[i] *= 2;
+        } else {
+//      Если матрица нестандартная, то проверяем вертикали-горизонтали-диагонали в зависимости от количества нужных фишек
+            for (int y = 0; y < matrix.length; y++) {
+                lineWin = 0;
+                for (int x = 0; x < matrix.length && lineWin < countBitForWin; x++)
+                    if (matrix[y][x] == sign) lineWin++; else lineWin = 0;
+                if (lineWin >= countBitForWin) return true;
+            }
 
-        formatPrint1DArray(arr);
-    }
+            for (int x = 0; x < matrix.length; x++) {
+                lineWin = 0;
+                for (int y = 0; y < matrix.length && lineWin < countBitForWin; y++)
+                    if (matrix[y][x] == sign) lineWin++; else lineWin = 0;
+                if (lineWin >= countBitForWin) return true;
+            }
 
-//    4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое), и с помощью цикла(-ов)
-//    заполнить его диагональные элементы единицами (можно только одну из диагоналей, если обе сложно). Определить элементы
-//    одной из диагоналей можно по следующему принципу: индексы таких элементов равны, то есть [0][0], [1][1], [2][2], …, [n][n];
+            lineWin = 0;
+            contrLineWin = 0;
+            for (int x = 0; x <= matrix.length - countBitForWin; x++) {
+                for (int y = 0; y < matrix.length - x && lineWin < countBitForWin && contrLineWin < countBitForWin; y++) {
+                    if (matrix[y][x + y] == sign) lineWin++;
+                    else lineWin = 0;
+                    if (matrix[y + x][y] == sign) contrLineWin++;
+                    else contrLineWin = 0;
+                }
+                if (lineWin >= countBitForWin || contrLineWin >= countBitForWin) return true;
+            }
 
-    public static void fillDiagonal(int len) {
-        int[][] arr = new int[len][len];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i][i] = 1;
-            arr[(arr.length - 1) - i][i] = 1;
-        }
-        print2DArray(arr);
-    }
-
-//    5. Написать метод, принимающий на вход два аргумента: len и initialValue, и возвращающий одномерный массив типа int
-//    длиной len, каждая ячейка которого равна initialValue;
-
-    public static int[] getInitArray(int len, int initialValue) {
-        int[] arr = new int[len];
-        for (int i = 0; i < arr.length; i++) arr[i] = initialValue;
-        return arr;
-    }
-
-//    6. * Задать одномерный массив и найти в нем минимальный и максимальный элементы ;
-
-    public static void printMinMax(int[] arr) {
-
-        print1DArray(arr);
-
-        int min = arr[0];
-        int max = arr[0];
-        for (int value: arr) {
-            if (value < min) min = value;
-            if (value > max) max = value;
-        }
-        System.out.println("Min = " + min + ", Max = " + max);
-    }
-
-//    7. ** Написать метод, в который передается не пустой одномерный целочисленный массив, метод должен вернуть true,
-//    если в массиве есть место, в котором сумма левой и правой части массива равны.
-
-    public static boolean checkBalance(int[] arr) {
-
-        print1DArray(arr);
-
-        int leftSum = 0;
-        int rightSum = 0;
-
-        for (int value: arr) rightSum += value;             //Считаем общую сумму -> rightSum
-        for (int i = 0; i < arr.length; i++) {
-            leftSum += arr[i];
-            rightSum -= arr[i];
-            if (leftSum == rightSum) {
-                for (int k = 0; k < arr.length; k++)        //Вывод массива с разделителем, затем return
-                    if (k == i) System.out.print(arr[k] + " || ");
-                    else System.out.print(arr[k] + " ");
-                System.out.println("\nSum = " + leftSum);
-                return true;
+            lineWin = 0;
+            contrLineWin = 0;
+            for (int x = 0; x <= matrix.length - countBitForWin; x++) {
+                for (int y = (matrix.length - x) -1; y >= 0 && lineWin < countBitForWin && contrLineWin < countBitForWin; y--) {
+                    if (matrix[y][((matrix.length -1) - x) - y] == sign) lineWin++;
+                    else lineWin = 0;
+                    if (matrix[((matrix.length -1) - x) - y][y] == sign) contrLineWin++;
+                    else contrLineWin = 0;
+                }
+                if (lineWin >= countBitForWin || contrLineWin >= countBitForWin) return true;
             }
         }
         return false;
     }
 
-//    8. *** Написать метод, которому на вход подается одномерный массив и число n (может быть положительным, или отрицательным),
-//    при этом метод должен сместить все элементы массива на n позиций. Элементы смещаются циклично.
-//    Для усложнения задачи нельзя пользоваться вспомогательными массивами. Примеры: [ 1, 2, 3 ] при n = 1 (на один
-//    вправо) -> [ 3, 1, 2 ]; [ 3, 5, 6, 1] при n = -2 (на два влево) -> [ 6, 1, 3, 5 ]. При каком n в какую сторону
-//    сдвиг можете выбирать сами.
+    static void moveAI(char[][] matrix) {
+        Random random = new Random();
+        int xCoord = 0;
+        int yCoord = 0;
+        boolean foundWin = false;
 
-    public static void shiftArray(int[] arr, int nShift) {
+//        Проверяем, может ли AI выиграть на этом шаге, если - да, то ходим туда
+        for (int y = 0; y < matrix.length && !foundWin; y++)
+            for (int x = 0; x < matrix.length && !foundWin; x++)
+                if (isEmptyCell(matrix, x, y)) {
+                    matrix[y][x] = signAI;
+                    if (isWin(matrix, signAI)) {
+                        xCoord = x;
+                        yCoord = y;
+                        foundWin = true;
+                    }
+                    matrix[y][x] = emptyChar;
+                }
 
-        print1DArray(arr);
-        int tmpVar;
+//      Проверяем, может ли человек выиграть на этом шаге, если - да, то блокируем его
+        for (int y = 0; y < matrix.length && !foundWin; y++)
+            for (int x = 0; x < matrix.length && !foundWin; x++)
+                if (isEmptyCell(matrix, x, y)) {
+                    matrix[y][x] = signHuman;
+                    if (isWin(matrix, signHuman)) {
+                        xCoord = x;
+                        yCoord = y;
+                        foundWin = true;
+                    }
+                    matrix[y][x] = emptyChar;
+                }
 
-        if (nShift > 0) {
-            System.out.println("(" + nShift + "->)");
-            for (int i = 1; i <= nShift; i++) {
-                tmpVar = arr[arr.length - 1];
-                for (int j = arr.length - 1; j > 0; j--) arr[j] = arr[j - 1];
-                arr[0] = tmpVar;
-            }
-        } else if (nShift < 0) {
-            System.out.println("(<-" + nShift + ")");
-            for (int i = -1; i >= nShift; i--) {
-                tmpVar = arr[0];
-                for (int j = 0; j < arr.length - 1; j++) arr[j] = arr[j + 1];
-                arr[arr.length - 1] = tmpVar;
-            }
-        }
+//        Если ничего интересного, то ходим по random
+        if (!foundWin)
+        do {
+            xCoord = random.nextInt(matrix.length);
+            yCoord = random.nextInt(matrix.length);
+        } while (!isEmptyCell(matrix, xCoord, yCoord));
 
-        print1DArray(arr);
+        matrix[yCoord][xCoord] = signAI;
+        System.out.printf("AI moved: X= %s; Y= %s.%n", xCoord + 1, yCoord + 1);
+        drawMatrix(matrix);
     }
 
-    public static void print1DArray(int[] arr) {
-        for (int value: arr) System.out.print(value + " ");
+    static boolean movePlayer(char[][] matrix) {
+        int xCoord = 0;
+        int yCoord = 0;
+
+        do {
+            xCoord = getCoordinate('X', matrix.length);
+            if (xCoord >= 0) {
+                yCoord = getCoordinate('Y', matrix.length);
+            }
+            if (xCoord < 0 || yCoord < 0) return false;
+        } while (!isEmptyCell(matrix, xCoord, yCoord));
+        matrix[yCoord][xCoord] = signHuman;
+        return true;
+    }
+
+    static boolean isEmptyCell(char[][] matrix, int xCoord, int yCoord) {
+        return (matrix[yCoord][xCoord] == emptyChar);
+    }
+
+    static int getCoordinate(char axis, int sizeOfMatrix) {
+        Scanner scanner = new Scanner(System.in);
+        int coordinate;
+
+        do {
+            System.out.printf("Enter %s-coordinate [in range 1-%s], or 0 to exit: ", axis, sizeOfMatrix);
+            coordinate = scanner.nextInt() - 1;
+        } while (coordinate < (-1) || coordinate >= sizeOfMatrix);
+        return coordinate;
+    }
+
+    static void drawMatrix(char[][] matrix) {
+        System.out.print("   |");
+        for (int i = 0; i < matrix.length; i++)
+            System.out.printf("  %s ", i + 1);
         System.out.println();
-    }
 
-    public static void print2DArray(int[][] arr) {
-        for (int[] iValue: arr) {
-            for (int jValue: iValue) System.out.print(jValue + " ");
+        for (int i = 0; i <= matrix.length; i++)
+            System.out.print("----");
+        System.out.println();
+
+        for (int j = 0; j < matrix.length; j++) {
+            System.out.printf("  %s|", j + 1);
+            for (int i = 0; i < matrix.length; i++)
+                System.out.printf("  %s ", matrix[j][i]);
             System.out.println();
         }
+        System.out.println();
     }
 
-    public static void formatPrint1DArray(int[] arr) {
-        for (int value: arr) System.out.format("%4d", value);
-        System.out.println();
+    static char[][] setMatrixEmpty(int sizeOfMatrix) {
+        char[][] matrix = new char[sizeOfMatrix][sizeOfMatrix];
+        for (int y = 0; y < matrix.length; y++)
+            for (int x = 0; x < matrix.length; x++)
+                matrix[y][x] = emptyChar;
+        return matrix;
+    }
+
+    static int setSizeOfMatrix() {
+        Scanner scanner = new Scanner(System.in);
+        int sizeOfMatrix;
+        do {
+            System.out.print("Enter size of matrix (3 or 5) or 0 to exit: ");
+            sizeOfMatrix = scanner.nextInt();
+        } while (sizeOfMatrix != 3 && sizeOfMatrix != 5 && sizeOfMatrix != 0);
+        return sizeOfMatrix;
     }
 }
